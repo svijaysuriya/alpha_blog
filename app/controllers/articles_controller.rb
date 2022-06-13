a512a0f310b3d97261a9973e60208d473e6aa81b
svijaysuriya <svijaysuriya09@gmail.com>
2022-06-13 14:14:57 +0530

    add articles listing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,4 +3,7 @@ class ArticlesController < ApplicationController
         # binding.break
         @article = Article.find(params[:id])
     end
+    def index
+      @articles = Article.all # inorder to be available at the views file!!
+    end
 end
