8dc981df4b1965c2fdd8cd6c98734317a7dd4e08
svijaysuriya <svijaysuriya09@gmail.com>
2022-06-16 07:00:58 +0530

    add users show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,14 @@
 class UsersController < ApplicationController
+
+    def show
+        @user = User.find(params[:id])
+        @articles = @user.articles
+    end
+
     def new
         @user = User.new
     end
+
     def create
         @user = User.new(user_params)
         if(@user.save)
@@ -11,9 +18,11 @@ class UsersController < ApplicationController
             render 'new'
         end
     end
+
     def edit
         @user = User.find(params[:id])
     end
+
     def update
         @user = User.find(params[:id])
         if @user.update(user_params)
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+    def gravatar_for (user,options={size:80})
+        email_add = user.email.downcase
+        hash = Digest::MD5.hexdigest(email_add)
+        s = options[:size]
+        gravatar_src = "https://www.gravatar.com/avatar/#{hash}?s=#{s}"
+        image_tag(gravatar_src,alt:user.username,class:"rounded shadow mx-auto d-block ")
+    end
 end
